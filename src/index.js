7efa0d2c867c70189e21dce753c63fcdf4620cad
sanderpiva <84524010+sanderpiva@users.noreply.github.com>
2026-05-06 14:09:28 +0000

    refactor 1: improving some archives  and testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à API de Gerenciamento de Produtos!');
+});
 
 app.listen(port, () => {
   console.log(`API rodando em http://localhost:${port}`);
